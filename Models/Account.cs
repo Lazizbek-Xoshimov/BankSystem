3bf6bac15d53c31a995ded14bb7812d5cf83803e
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-24 15:09:42 +0500

    Deposit, withdrawal and balance viewing methods added

--- Models/Account.cs
+++ Models/Account.cs
@@ -4,14 +4,24 @@ public abstract class Account
 {
     public string OwnerName { get; set; }
     public decimal Balance { get; set; }
-
-    public Account()
-    {
-    }
     
     public Account(string ownerName, decimal balance)
     {
         this.OwnerName = ownerName;
         this.Balance = balance;
+    }
+
+    public abstract void Withdraw(decimal amount);
+
+    public decimal Deposit(decimal amount)
+    {
+        this.Balance += amount;
+
+        return this.Balance;
+    }
+    
+    public virtual void ShowBalance()
+    {
+        Console.WriteLine($"Your balance: {this.Balance}");
     }
 }
