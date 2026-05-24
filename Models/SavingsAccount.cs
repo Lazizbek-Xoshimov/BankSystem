f5e85486add6ff92e4ae1510fca2f0edac788897
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-24 16:57:55 +0500

    ShowBalance has been overriden and a change has been made to Withdraw

--- Models/SavingsAccount.cs
+++ Models/SavingsAccount.cs
@@ -6,11 +6,24 @@ public class SavingsAccount : Account
     {
     }
 
-    public override void Withdraw(decimal amount)
+    public override decimal Withdraw(decimal amount)
     {
+        Console.WriteLine($"You have sent a withdrawal request for ${amount}.");
+        Console.Write("This is the answer to the request: ");
+
         if (Balance < amount)
             Console.WriteLine("Not enough balance.");
-        else 
+        else
+        {
             Balance -= amount;
+            Console.WriteLine("Successful.");
+        }
+
+        return Balance;
+    }
+
+    public override void ShowBalance()
+    {
+        Console.WriteLine($"You have ${Balance} in your savings account.");
     }
 }
